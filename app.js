14b2504e631bff48fc339d5a96190a45eff8473a
Nikita rungta <j07nikita@gmail.com>
2020-08-03 21:30:24 +0530

    join room and create room added

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const app = require('express')();
 const server = require('http').Server(app);
-
+const path = require('path');
 const { port } = require('./config');
 
 let roomManager = require('./app/controllers/room')
 let playerManager = require('./app/controllers/player')
+console.log(roomManager);
+console.log(playerManager);
 
+var url = require('url');
+var bodyParser = require('body-parser');
+
+
+app.use(require('express').static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 server.listen(port, () => {
     console.log(`Server running at port:${port}`);
 });
-
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/app/views/index.html');
 });
 
 app.post('/createRoom', function (req, res) {
+	console.log(req.body);
     let room = roomManager.createRoom(req.body.noOfRounds, req.body.timeToGuess);
     let player = playerManager.createPlayer(req.body.playerName, true);
     roomManager.addPlayerToRoom(room.roomName, player);
@@ -27,4 +35,14 @@ app.post('/createRoom', function (req, res) {
         roomName: room.roomName,
     };
     res.send(data);
+});
+
+app.post('/joinRoom', function (req, res) {
+    console.log(req.body);
+    let player = playerManager.createPlayer(req.body.playerName, false);
+    let status = roomManager.addPlayerToRoom(req.body.roomName, player);
+    console.log(status);
+    let data = { status: status };
+    res.status(status);
+    res.send(JSON.stringify(data));
 });
