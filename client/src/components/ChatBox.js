056f7e146bcbf09ad366caec56803d1674ac1594
eeshadutta <eeshadutta99@gmail.com>
2020-08-08 02:21:50 +0530

    Add chatbox

---
+++ client/src/components/ChatBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ChatBox() {
+    return (
+        <div style={{ padding: 10 }}>
+            <h3 style={{ marginTop: 10, backgroundColor: 'lightgray', padding: 8, textAlign: "center" }}>
+                Chat
+            </h3>
+        </div>
+    );
+}
+
+export default ChatBox;
