aa080423dde4bc8305ddb62b1b92b9f42797cad6
eeshadutta <eeshadutta99@gmail.com>
2020-08-08 01:18:31 +0530

    Add scoreboard area

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import { TextField, Hidden } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import Canvas from './components/Canvas';
+import ScoreBoard from './components/ScoreBoard';
 import CanvasDraw from "react-canvas-draw";
 
 function App() {
   return (
     <Grid container className="layoutContainer">
-      {/* <Grid item md={3} lg={3}>
+      <Grid item md={3} lg={3}>
+        <ScoreBoard />
       </Grid>
 
-      <Grid item md={6} lg={6}> */}
-        {/* <Canvas /> */}
+      <Grid item md={6} lg={6}>
         <Canvas />
-      {/* </Grid>
+      </Grid>
 
       <Grid item md={3} lg={3}>
-      </Grid> */}
+      </Grid>
 
     </Grid>
   );
