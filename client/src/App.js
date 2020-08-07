39f471d27e04a659c1a4173a3760dcb755705548
eeshadutta <eeshadutta99@gmail.com>
2020-08-08 02:21:34 +0530

    Add timer

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import './App.css';
-import { TextField, Hidden } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
+import { Grid } from '@material-ui/core';
 import Canvas from './components/Canvas';
 import ScoreBoard from './components/ScoreBoard';
-import CanvasDraw from "react-canvas-draw";
+import Timer from './components/Timer';
+import ChatBox from './components/ChatBox';
+import CanvasDraw from 'react-canvas-draw';
 
 function App() {
-  return (
-    <Grid container className="layoutContainer">
-      <Grid item md={3} lg={3}>
-        <ScoreBoard />
-      </Grid>
+    return (
+        <Grid container className="layoutContainer">
+            <Grid item md={3} lg={3}>
+                <ScoreBoard />
+            </Grid>
 
-      <Grid item md={6} lg={6}>
-        <Canvas />
-      </Grid>
+            <Grid item md={6} lg={6}>
+                <Canvas />
+            </Grid>
 
-      <Grid item md={3} lg={3}>
-      </Grid>
+            <Grid item md={3} lg={3}>
+                <Timer />
+                <ChatBox />
+            </Grid>
 
-    </Grid>
-  );
+        </Grid>
+    );
 }
 
 export default App;
