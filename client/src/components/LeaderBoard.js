33d1d1cf0a1372bbdbf5a3ce9268b29f262db2db
Eesha Dutta <eeshadutta27@gmail.com>
2020-08-25 22:50:28 +0530

    Add rank

--- client/src/components/LeaderBoard.js
+++ client/src/components/LeaderBoard.js
@@ -6,7 +6,7 @@ export default class LeaderBoard extends React.Component {
     constructor() {
         super();
         this.state = {
-            playersList: [{ username: "player1", points: 80 }, { username: "player2", points: 50 }],
+            playersList: [{ rank: 1, username: "player1", points: 80 }, { rank: 2, username: "player2", points: 50 }],
         }
     }
 
@@ -18,12 +18,13 @@ export default class LeaderBoard extends React.Component {
                 <Grid item md={4} lg={4}>
                     <div className="board">
                         <h1 className="title">Game Over</h1>
-                        <img src="https://i.pinimg.com/originals/44/d5/ff/44d5ff705fd223c4cddd1cdc1e2eefa1.jpg" alt="" style={{ width: 90, height: 60}}></img>
+                        <img src="https://i.pinimg.com/originals/44/d5/ff/44d5ff705fd223c4cddd1cdc1e2eefa1.jpg" alt="" style={{ width: 90, height: 60 }}></img>
                         <h2>Leaderboard</h2>
                         <Table>
                             <TableBody>{this.state.playersList.map(function (item, key) {
                                 return (
                                     <TableRow key={key} className="row">
+                                        <TableCell style={{ textAlign: "center", fontSize: 30 }}>#{item.rank}</TableCell>
                                         <TableCell style={{ textAlign: "center", fontSize: 30 }}>{item.username}</TableCell>
                                         <TableCell style={{ textAlign: "center", fontSize: 30 }}>{item.points}</TableCell>
                                     </TableRow>
